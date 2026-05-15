c9c02a2d54c2e85f651a469a86075e34909f4f94
Atour Mousavi Gourabi <28668597+amousavigourabi@users.noreply.github.com>
2026-05-16 00:53:57 +0200

    Optimize CSS bundle

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ compress: true })
+		adapter: adapter({ compress: true }),
+		output: {
+			bundleStrategy: 'split'
+		}
 	}
 };
 
